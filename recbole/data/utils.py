cf077500b29e896cf2c8145664f3a913a0a81f40
CubicPower <55009831+markhsia@users.noreply.github.com>
2023-09-16 13:36:51 +0800

    Update utils.py

--- recbole/data/utils.py
+++ recbole/data/utils.py
@@ -17,7 +17,8 @@ import importlib
 import os
 import pickle
 import warnings
-from typing import Literal
+#from typing import Literal
+from typing_extensions import Literal
 
 from recbole.data.dataloader import *
 from recbole.sampler import KGSampler, Sampler, RepeatableSampler
